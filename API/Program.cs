4de0729f0e5c3d26e004cdbc21c66c69e415c837
Nataliia Divchur <nataliadivchur@gmail.com>
2023-10-05 18:21:58 +0300

    Angular skeleton

--- API/Program.cs
+++ API/Program.cs
@@ -10,14 +10,15 @@ builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
-
+builder.Services.AddCors();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
-app.UseAuthorization();
+//app.UseAuthorization();
 
 app.MapControllers();
 
