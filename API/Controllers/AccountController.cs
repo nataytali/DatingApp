4ecaa269cde7394dd17d27ec01365bf2001f546c
Nataliia Divchur <nataliadivchur@gmail.com>
2023-11-29 17:40:09 +0200

    Added photo upload, set main, delete functionality

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
@@ -43,15 +44,17 @@ namespace API.Controllers
             return new UserDto
             {
                 Username = user.UserName,
-                Token = _tokenService.CreateToken(user)
+                Token = _tokenService.CreateToken(user),
+                PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url
             };
         }
 
         [HttpPost("login")]
         public async Task<ActionResult<UserDto>> Login (LoginDto loginDto)
         {
-            var user = await _context.Users.SingleOrDefaultAsync( x => 
-                x.UserName == loginDto.Username);
+            var user = await _context.Users
+                .Include(p => p.Photos)
+                .SingleOrDefaultAsync( x => x.UserName == loginDto.Username);
             
             if (user == null) return Unauthorized("Invalid username");
 
@@ -67,7 +70,8 @@ namespace API.Controllers
              return new UserDto
             {
                 Username = user.UserName,
-                Token = _tokenService.CreateToken(user)
+                Token = _tokenService.CreateToken(user),
+                PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url
             };
         }
 
--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -19,8 +19,7 @@ export class AccountService {
       map(response => {
         const user = response;
         if(user){
-          localStorage.setItem('user', JSON.stringify(user)); // to not login every time we update page
-          this.currentUserSource.next(user);
+          this.setCurrentUser(user); // to not login every time we update page
         }
 
         return user;
@@ -32,8 +31,7 @@ export class AccountService {
     return this.http.post<User>(this.baseUrl + 'account/register', model).pipe(
       map(user => {
         if(user){
-          localStorage.setItem('user', JSON.stringify(user));
-          this.currentUserSource.next(user);
+          this.setCurrentUser(user);
         }
         return user;
       })
@@ -41,6 +39,7 @@ export class AccountService {
   }
 
   setCurrentUser(user: User) {
+    localStorage.setItem('user', JSON.stringify(user));
     this.currentUserSource.next(user)
   }
 
