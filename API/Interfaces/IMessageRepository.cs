9e56af0ffa0bc9bed972ff795e9ae46b34b97cab
Nataliia Divchur <nataliadivchur@gmail.com>
2024-01-17 19:09:06 +0200

    Adding a messaging feature

---
+++ API/Interfaces/IMessageRepository.cs
@@ -0,0 +1,17 @@
+using API.Entities;
+using API.Helpers;
+using API.DTOs;
+using Newtonsoft.Json.Linq;
+
+namespace API.Interfaces
+{
+    public interface IMessageRepository
+    {
+        void AddMessage(Message message);
+        void DeleteMessage(Message message);
+        Task<Message> GetMessage(int id);
+        Task<PagedList<MessageDto>> GetMessagesForUser(MessageParams messageParams);
+        Task<IEnumerable<MessageDto>> GetMessageThread(string currentUsername, string recipientUsername);
+        Task<bool> SaveAllAsync();
+    }
+}
